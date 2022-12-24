a915919b843161333ac0788ccb680411627a7700
mehdiye5 <greg.mehdiyev@gmail.com>
2022-12-24 15:50:28 -0800

    Fixed errors

--- sniffer/bert.py
+++ sniffer/bert.py
@@ -41,6 +41,7 @@ class Dataset(torch.utils.data.Dataset):
 
         return batch_texts, batch_y
 
+
 class BertClassifier(nn.Module):
     """
     BERT encoder model with custom feed forward architecture.
@@ -66,3 +67,126 @@ class BertClassifier(nn.Module):
         x = self.output(x)
         final_layer = self.sigmoid(x)
         return final_layer
+
+def bert_train(model, train_data, val_data, tokenizer):
+    """
+    Training function for the BERT classifier
+    
+    Args
+    ---------
+        model: BERT classifier model
+        train_data: pandas df
+        val_data: pandas df
+        tokenizer: BERT tokenizer
+    """
+
+    EPOCHS = 10  
+    LR = 1e-3
+    BATCH_SIZE = 1024
+
+    train, val = Dataset(train_data, tokenizer), Dataset(val_data, tokenizer)
+
+    train_dataloader = torch.utils.data.DataLoader(train, batch_size=BATCH_SIZE, shuffle=True)
+    val_dataloader = torch.utils.data.DataLoader(val, batch_size=BATCH_SIZE)
+
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = Adam(model.parameters(), lr=LR)
+
+    if use_cuda:
+        model = model.cuda()
+        criterion = criterion.cuda()
+
+    for epoch_num in range(EPOCHS):
+
+        total_acc_train = 0
+        total_loss_train = 0
+
+        for train_input, train_label in tqdm(train_dataloader):
+
+            train_label = train_label.to(device)
+            mask = train_input['attention_mask'].to(device)
+            input_id = train_input['input_ids'].squeeze(1).to(device)
+
+            output = model(input_id, mask)
+            
+            batch_loss = criterion(output, train_label.long())
+            total_loss_train += batch_loss.item()
+            
+            acc = (output.argmax(dim=1) == train_label).sum().item()
+            total_acc_train += acc
+
+            model.zero_grad()
+            batch_loss.backward()
+            optimizer.step()
+        
+        total_acc_val = 0
+        total_loss_val = 0
+
+        with torch.no_grad():
+
+            for val_input, val_label in val_dataloader:
+
+                val_label = val_label.to(device)
+                mask = val_input['attention_mask'].to(device)
+                input_id = val_input['input_ids'].squeeze(1).to(device)
+
+                output = model(input_id, mask)
+
+                batch_loss = criterion(output, val_label.long())
+                total_loss_val += batch_loss.item()
+                
+                acc = (output.argmax(dim=1) == val_label).sum().item()
+                total_acc_val += acc
+        
+        print(
+            f'Epochs: {epoch_num + 1} | Train Loss: {total_loss_train / len(train_data): .3f} \
+            | Train Accuracy: {total_acc_train / len(train_data): .3f} \
+            | Val Loss: {total_loss_val / len(val_data): .3f} \
+            | Val Accuracy: {total_acc_val / len(val_data): .3f}')
+    
+    torch.save(model.state_dict(), 'bert_model.pth')
+
+def bert_evaluate(model, test_data, tokenizer):
+    """
+    Evaluates the performance of BERT on the test set
+    
+    Args
+    ---------
+        model: BERT classifier model
+        test_data: pandas df
+        tokenizer: BERT tokenizer
+    """
+
+    test = Dataset(test_data, tokenizer)
+    test_dataloader = torch.utils.data.DataLoader(test, batch_size=2)
+
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    criterion = nn.CrossEntropyLoss()
+
+    if use_cuda:
+        model = model.cuda()
+        criterion = criterion.cuda()
+
+    total_acc_test = 0  
+    loss = 0
+    with torch.no_grad():
+        for test_input, test_label in test_dataloader:
+
+              test_label = test_label.to(device)
+              mask = test_input['attention_mask'].to(device)
+              input_id = test_input['input_ids'].squeeze(1).to(device)
+
+              output = model(input_id, mask)
+
+              batch_loss = criterion(output, test_label.long())
+              loss += batch_loss.item()
+
+              acc = (output.argmax(dim=1) == test_label).sum().item()
+              total_acc_test += acc              
+    
+    print(f'Test Accuracy: {total_acc_test / len(test_data): .4f}')
+    print(f'Test CCE Loss: {loss / len(test_data): .4f}')
